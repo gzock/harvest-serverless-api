83f663fbeb02dd1fbc071966240fbff7ab578f6c
gzock <gzock@example.com>
2019-02-10 16:25:36 +0000

    createの実装(90%ぐらい)

--- places/test.py
+++ places/test.py
@@ -17,11 +17,11 @@ try:
 
   place = PlaceController(DYNAMO_HOST, DYNAMO_PORT)
 
-  place.set_project_id("9cb83d38-4950-4709-ad9d-7eeed2664ddc")
+  place.set_project_id("18873c7a-0fcb-4d85-b067-d9ed7dc184cc")
   #place.create("root_place")
   #print( place.list_children("9cb83d38-4950-4709-ad9d-7eeed2664ddc") )
-  #place.create("first_place_2", "8003532a-2122-44ff-95c2-5e5276b45453")
-  print( place.list_children("8003532a-2122-44ff-95c2-5e5276b45453") )
+  place.create("first_place_2", "7974dde1-ec16-4822-a499-ff73fd74d5cb")
+  #print( place.list_children("8003532a-2122-44ff-95c2-5e5276b45453") )
   
   #place.set_place_id("f58d7b86-0b4a-442f-9ee1-79635b43b4bf")
   #place.update_name("first_place_renamed")
