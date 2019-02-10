275642b9bf64cf35e5a85b5ce1546bb35a58e176
gzock <gzock@example.com>
2019-02-10 14:19:37 +0000

    targetsのtable作成を実装

--- create_tables.sh
+++ create_tables.sh
@@ -44,3 +44,19 @@ aws dynamodb create-table \
     --local-secondary-indexes IndexName=ParentPlacesIndex,KeySchema=["{AttributeName=project_id,KeyType=HASH}","{AttributeName=parent_place_id,KeyType=RANGE}"],Projection="{ProjectionType=ALL}" \
     --provisioned-throughput ReadCapacityUnits=3,WriteCapacityUnits=3 
 echo "----------"
+
+echo "`date`  create targets table"
+echo "----------"
+aws dynamodb create-table \
+    --endpoint-url http://localhost:8000 \
+    --table-name Targets \
+    --attribute-definitions \
+        AttributeName=project_id,AttributeType=S \
+        AttributeName=target_id,AttributeType=S \
+        AttributeName=parent_place_id,AttributeType=S \
+    --key-schema \
+        AttributeName=project_id,KeyType=HASH \
+        AttributeName=target_id,KeyType=RANGE \
+    --local-secondary-indexes IndexName=ParentPlacesIndex,KeySchema=["{AttributeName=project_id,KeyType=HASH}","{AttributeName=parent_place_id,KeyType=RANGE}"],Projection="{ProjectionType=ALL}" \
+    --provisioned-throughput ReadCapacityUnits=3,WriteCapacityUnits=3 
+echo "----------"
