c19d580e9b8fba3cc2a9a4e1b9b68c64c278b80c
gzock <gzock@example.com>
2019-02-11 06:01:53 +0000

    targets-apiを実装

--- routes/targets/targets.py
+++ routes/targets/targets.py
@@ -1,14 +1,12 @@
 import os, sys
 import json
-from botocore.vendored import requests
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'site-packages'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../site-packages'))
 
 import boto3
-from boto3.dynamodb.conditions import Key, Attr
 import logging
 
-from harvest import RequestDecorator, PlaceController
+from harvest import RequestDecorator, TargetController
 
 DYNAMO_HOST = "10.0.2.15"
 DYNAMO_PORT = "8000"
@@ -18,7 +16,7 @@ def lambda_handler(event, context):
   logger.setLevel(logging.INFO)
 
   try:
-    project = PlaceController(DYNAMO_HOST, DYNAMO_PORT)
+    target = TargetController(DYNAMO_HOST, DYNAMO_PORT)
     req = RequestDecorator(event)
   except Exception as e:
     raise e
@@ -27,17 +25,30 @@ def lambda_handler(event, context):
   # prod
   # project.set_user_id(user_id)
   # dev
-  project.set_user_id("ryo_sasaki")
+  #place.set_user_id("ryo_sasaki")
   logger.info("requested user id: {}".format(user_id))
 
   username = req.get_username()
   logger.info("requested user name: {}".format(username))
 
-  project_id = req.get_path_param() #uuidの確認
-  if project_id:
-    project.set_project_id(project_id)
-  logger.info("requested project_id: {}".format(project_id))
+  path_params = req.get_path_params()
+  if "project_id" in path_params:
+    project_id = path_params["project_id"]
+    target.set_project_id(project_id)
+    logger.info("requested project_id: {}".format(project_id))
+
+  if "place_id" in path_params:
+    place_id = path_params["place_id"]
+    logger.info("requested target_id: {}".format(place_id))
+
+  if "target_id" in path_params:
+    target_id = path_params["target_id"]
+    target.set_target_id(target_id)
+    logger.info("requested target_id: {}".format(target_id))
+
   logger.info("requested http method: {}".format(req.get_method()))
+  logger.info("requested path: {}".format(req.get_path()))
+  logger.info("requested pathParams: {}".format(req.get_path_params()))
   
   status_code = 200
   headers = {
@@ -45,24 +56,29 @@ def lambda_handler(event, context):
       "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PUT.DELETE",
       "Access-Control-Allow-Origin": "*"
   }
-  # /projects
-  if req.get_method() == "GET":
-    # /projects/xxxx-xxxx-xxxx-xxxx
-    if project_id:
-      ret = project.show()
-    else:
-      ret = project.list_projects()
 
-  # /projects
+  if req.get_method() == "GET" and target_id:
+    # /projects/{project_id}/targets/{target_id}
+    ret = target.show(target_id)
+
   elif req.get_method() == "POST":
     name = req.get_body()["name"]
-    ret = project.create(name)
+    # /projects/{project_id}/places/{place_id}/targets
+    if place_id:
+      ret = target.create(name, place_id)
 
-  elif req.get_method() == "PUT":
-    ret = project.update(project_id, body)
+    # /projects/{project_id}/targets
+    else:
+      ret = target.create(name)
 
-  elif req.get_method() == "DELETE":
-    ret = project.delete(project_id)
+  # /projects/{project_id}/targets/{target_id}
+  elif req.get_method() == "PUT" and target_id:
+    name = req.get_body()["name"]
+    ret = target.update(project_id, name)
+
+  # /projects/{project_id}/targets/{target_id}
+  elif req.get_method() == "DELETE" and target_id:
+    ret = target.delete(project_id)
 
   elif req.get_method() == "OPTIONS":
     ret = []
