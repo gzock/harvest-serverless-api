690b01e5b1869196559e0d2073710d9a2770cb7b
gzock <gzock0308@hotmail.co.jp>
2019-03-10 06:19:55 +0000

    GenerateをAPI gatewayで使用できるように実装

--- routes/generate/generate.py
+++ routes/generate/generate.py
@@ -3,7 +3,7 @@ import json
 import logging
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../site-packages'))
-from harvest import RequestDecorator, TargetController
+from harvest import RequestDecorator, Generate
 
 #DYNAMO_HOST = "10.0.2.15"
 #DYNAMO_PORT = "8000"
@@ -15,7 +15,7 @@ def lambda_handler(event, context):
   logger.setLevel(logging.INFO)
 
   try:
-    target = TargetController(DYNAMO_HOST, DYNAMO_PORT)
+    gen = Generate(DYNAMO_HOST, DYNAMO_PORT)
     req = RequestDecorator(event)
   except Exception as e:
     raise e
@@ -33,17 +33,11 @@ def lambda_handler(event, context):
   path_params = req.get_path_params()
   if "project_id" in path_params:
     project_id = path_params["project_id"]
-    target.set_project_id(project_id)
     logger.info("requested project_id: {}".format(project_id))
 
-  if "place_id" in path_params:
-    place_id = path_params["place_id"]
-    logger.info("requested target_id: {}".format(place_id))
-
-  if "target_id" in path_params:
-    target_id = path_params["target_id"]
-    target.set_target_id(target_id)
-    logger.info("requested target_id: {}".format(target_id))
+  if "type" in path_params:
+    gen_type = path_params["type"]
+    logger.info("requested generate type: {}".format(gen_type))
 
   logger.info("requested http method: {}".format(req.get_method()))
   logger.info("requested path: {}".format(req.get_path()))
@@ -56,31 +50,17 @@ def lambda_handler(event, context):
       "Access-Control-Allow-Origin": "*"
   }
 
-  if req.get_method() == "GET" and target_id:
-    # /projects/{project_id}/targets/{target_id}
-    ret = target.show(target_id)
+  if req.get_method() == "GET":
+    pass
 
   elif req.get_method() == "POST":
     name = req.get_body()["name"]
-    # /projects/{project_id}/places/{place_id}/targets
-    if "place_id" in locals():
-      ret = target.create(name, place_id)
-
-    # /projects/{project_id}/targets
-    else:
-      ret = target.create(name)
-
-  # /projects/{project_id}/targets/{target_id}
-  elif req.get_method() == "PUT" and target_id:
-    name = req.get_body()["name"]
-    ret = target.update_name(name)
-
-  # /projects/{project_id}/targets/{target_id}
-  elif req.get_method() == "DELETE" and target_id:
-    ret = target.delete(project_id)
-
-  elif req.get_method() == "OPTIONS":
-    ret = []
+    # /projects/{project_id}/generate/{type}
+    if gen_type in ["zip", "doc"]:
+      ret = gen.create_download_link(
+          project_id=project_id,
+          gen_type=gen_type, 
+      )
 
   return {
       "statusCode": status_code,
