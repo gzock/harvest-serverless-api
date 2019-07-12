7adff637f0b263d03881f2195f34ee8634be030f
gzock <gzock@example.com>
2019-07-12 03:22:10 +0000

    法人アカウント関係APIの作り始め

---
+++ routes/corporation/corporration.py
@@ -0,0 +1,73 @@
+import os, sys
+import json
+import logging
+import traceback
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../site-packages'))
+from harvest import RequestDecorator
+from harvest import Corporation
+from harvest import ActionDeniedError
+from harvest.utils.make_response_utils import make_response
+
+DYNAMO_HOST = os.environ.get("DYNAMO_HOST")
+DYNAMO_PORT = os.environ.get("DYNAMO_PORT")
+
+def lambda_handler(event, context):
+  formatter = logging.Formatter('[%(levelname)s]\t%(asctime)s.%(msecs)dZ\t%(aws_request_id)s\t[%(module)s#%(funcName)s %(lineno)d]\t%(message)s')
+
+  logger = logging.getLogger()
+  for handler in logger.handlers:
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.DEBUG)
+
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.DEBUG)
+
+  try:
+    corp = Corporation(DYNAMO_HOST, DYNAMO_PORT)
+    req = RequestDecorator(event)
+  except Exception as e:
+    raise e
+
+  corporation_id = req.get_identity_id()
+  #username = req.get_username()
+  if corporation_id:
+    corp.set_corporation_id(corporation_id)
+
+  path_params = req.get_path_params()
+  
+  status_code = 200
+  ret=""
+  # /corps
+  try:
+    if req.get_method() == "GET":
+      # /corps/xxxx-xxxx-xxxx-xxxx
+      ret = corp.show_corp()
+
+    # /corps
+    elif req.get_method() == "POST":
+      ret = corp.create_corp(name, start_on, complete_on)
+
+    elif req.get_method() == "PUT":
+      name = req.get_body()["name"]
+      ret = corp.update_corp(name, start_on, complete_on)
+
+    elif req.get_method() == "DELETE":
+      ret = corp.delete_corp()
+
+    elif req.get_method() == "OPTIONS":
+      ret = []
+    logger.info("processing successfully. return value: {}".format(ret))
+
+  except ActionDeniedError as e:
+    status_code = 403
+    ret = e
+    logger.error("permission denied.")
+    logger.exception(e)
+  except Exception as e:
+    status_code = 400
+    ret = e
+    logger.error("invalid request?? processing failed...")
+    logger.exception(e)
+
+  return make_response(status_code=status_code, body=ret)
